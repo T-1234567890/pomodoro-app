c08b8a55193cb1bc2413e2e6b006fd3106950fe6
T-1234567890 <tonyhu2111@gmail.com>
2026-01-15 21:20:23 +0800

    feat: initialize native macOS SwiftUI app

---
+++ macos/Pomodoro/Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Zhengyang Hu on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
